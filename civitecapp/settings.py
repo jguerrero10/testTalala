e911cab8ef8b0b16c59dcfa47d223c9b651f407f
Joel Guerrero <joel.guerrero@canalclima.com>
2023-01-19 20:35:43 -0500

    Readme is add

--- civitecapp/settings.py
+++ civitecapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^-&m##a$l)xbwizjeuk-7buwbu5e0ko!o^2bri%mc##qdqu1a%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
